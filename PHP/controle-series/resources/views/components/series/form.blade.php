3e741b93b9206ef56853a6083fd3fdcfb3e06351
Josué Carlos <josuecarlosos2@gmail.com>
2023-01-16 14:39:39 -0300

    PROJETO COMPARATIVO --------

---
+++ PHP/controle-series/resources/views/components/series/form.blade.php
@@ -0,0 +1,26 @@
+<form action="{!! $action !!}" method="post">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @csrf
+    @if($update)
+        @method('PUT')
+    @endif
+    <div class="mb-3">
+        <label for="nome" class="form-label">Nome:</label>
+        <input type="text"
+               id="nome"
+               name="nome"
+               class="form-control"
+               @isset($nome)value="{{ $nome }}"@endisset>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Adicionar</button>
+</form>
